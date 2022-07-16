14de1fe21206bd7d6894694feb67a1e8e6bd3762
FangtaiDong10 <fangtaidong10@gmail.com>
2022-07-16 18:16:10 +1000

    course module model and controller updated

---
+++ app/course/controller.py
@@ -0,0 +1,32 @@
+import datetime
+from email import message
+from flask_restx import Namespace, Resource
+from flask import request, jsonify
+
+from app.user import permission_required
+from .model import Course
+
+from flask_jwt_extended import create_access_token, current_user, jwt_required
+from ..utils import paginate
+
+
+course_api = Namespace("courses")
+
+
+@course_api.route("/")
+class CourseListApi(Resource):
+    @permission_required()
+    def get(self):
+        query = {}
+        if "campus" in request.args:
+            query["campus"] = request.args["campus"]
+
+        page = request.args.get("page", 1, int)
+
+        return paginate(Course.objects(**query), page)
+
+    @permission_required()
+    def post(self):
+        course = Course.from_json(request.data)
+        course.save()
+        return course.to_dict(), 201
---
+++ app/course/model.py
@@ -0,0 +1,41 @@
+from mongoengine import (
+    StringField,
+    ReferenceField,
+    ListField,
+    CASCADE,
+    DateTimeField,
+    FloatField,
+    EmbeddedDocument,
+    EmbeddedDocumentField,
+)
+from flask_mongoengine import Document
+from datetime import datetime
+
+
+class Course(Document):
+    name = StringField(required=True, max_length=200)
+    uni_course_code = StringField(required=True, max_length=200)
+    description = StringField(required=True, max_length=200)
+    teacher = ReferenceField("Teacher", reverse_delete_rule=CASCADE)
+    campus = ReferenceField("Campus", reverse_delete_rule=CASCADE)
+    created_time = DateTimeField(default=datetime.now)
+    publish_time = DateTimeField(default=datetime.now)
+    original_price = FloatField()
+    cover_image = StringField()
+
+    def to_dict(self):
+        return {
+            "id": str(self.id),
+            "name": self.name,
+            "uni_course_code": self.uni_course_code,
+            "description": self.description,
+            "teacher": {
+                "id": str(self.teacher.id),
+                "display_name": self.teacher.display_name,
+            },
+            "campus": self.campus.to_dict(),
+            "created_time": self.created_time.isoformat(),
+            "publish_time": self.publish_time.isoformat(),
+            "original_price": self.original_price,
+            "cover_image": self.cover_image,
+        }
