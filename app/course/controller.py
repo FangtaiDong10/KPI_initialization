78e786012ddd8360d5eff6b3013e5429d8f22b49
FangtaiDong10 <fangtaidong10@gmail.com>
2022-11-13 13:30:37 +1100

    lecture attachments API update

--- app/course/controller.py
+++ app/course/controller.py
@@ -9,6 +9,7 @@ from .model import Course, Lecture, LectureAttachment
 from flask_jwt_extended import create_access_token, current_user, jwt_required
 from ..utils import paginate
 from datetime import datetime
+import json
 
 course_api = Namespace("courses")
 
@@ -58,3 +59,47 @@ class LectureListApi(Resource):
         
         course.save()
         return lecture.to_dict(), 201
+
+    @jwt_required()
+    def get(self, course_id):
+        course = Course.objects(id=course_id).first_or_404()
+
+        if current_user._cls == "User.Student":
+            print(course.enrolled_students)
+            if current_user not in course.enrolled_students:
+                return {"messege": "You are not allowed to access this course"}, 403
+        
+        return [lecture.to_dict() for lecture in course.lectures]
+
+
+# for uploading the attachment by the teacher
+@course_api.route("/<course_id>/lectures/<lecture_id>/attachments")
+class LectureAttachmentApi(Resource):
+    @permission_required("lecture_admin")
+    def post(self, course_id, lecture_id):
+        course = Course.objects(id=course_id).first_or_404()
+        
+        # original api therefore no first_or_404
+        lecture = course.lectures.filter(id=lecture_id).first()
+
+        if lecture is None:
+            return {"message": "lecture not exists"}, 404
+        
+        # if the lecture exists, then the attachment should be uploaded
+        uploaded_file = request.files.get("file")
+
+        if uploaded_file is None:
+            return {"message": "No file uploaded"}, 400
+
+        if uploaded_file.filename in [attachment.filename for attachment in lecture.attachments]:
+            return {"message": "File already exists"}, 409
+        
+        # Upload logic
+        attachment = LectureAttachment(
+            **json.loads(request.form.get("metadata", default={})), 
+            filename = uploaded_file.filename
+        )
+        lecture.attachments.append(attachment)
+
+        # print(uploaded_file)
+        return attachment.to_dict() , 201
